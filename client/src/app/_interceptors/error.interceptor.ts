52c1dbf0c06ea5617b898a24b1c84d26e99571a9
sebi <sebastian.heri@outlook.com>
2024-12-29 08:58:45 +0100

    Initialize

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
+  return next(req);
+};
